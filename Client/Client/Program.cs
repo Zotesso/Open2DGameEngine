27d4efe2f362fbb4b25dac1c2b8ef45d241bae32
Zotesso <siriothart@gmail.com>
2020-10-06 14:05:57 -0300

    Game Window Opened!

---
+++ Client/Client/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SFML.Graphics;
+
+namespace Client
+{
+    class Program
+    {
+        public static RenderWindow gameWindow;
+        static void Main(string[] args)
+        {
+            gameWindow = new RenderWindow(new SFML.Window.VideoMode(800, 600), "Game Engine");
+            Game();
+        }
+
+        static void Game()
+        {
+            while (gameWindow.IsOpen)
+            {
+                gameWindow.DispatchEvents();
+                gameWindow.Clear(Color.Red);
+
+                gameWindow.Display();
+            }
+        }
+    }
+}
